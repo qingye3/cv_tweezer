8235e27deff15c16ad07cb3f9e2f46a6736debe0
qingye3 <qingye3@illlinois.edu>
2014-03-04 11:44:42 -0600

    update

--- cv_tweezer.py
+++ cv_tweezer.py
@@ -12,7 +12,7 @@ class Frame:
         self.center = None
         self.radius = None
     def read(self):
-        self.image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        self.image = cv2.imread(self.path, cv2.IMREAD_GRAYSCALE)
     def obtain_center(self):
         self.protocol(self)
     def save(self, new_path = 'default'):
@@ -34,9 +34,14 @@ class Animation:
     def import_frame(self, path, protocol):
         self.frames.append(Frame(path, protocol))
     def import_seq(self, path, regex, protocol):
+        self.frames = []
+        indice = []
         for file_name in os.listdir(path):
-            if re.match(regex, file_name) != None:
+            reg_match = re.match(regex, file_name)
+            if reg_match != None:
                 self.frames.append(Frame(path+file_name, protocol))
+                indice.append(int(reg_match.group(1)))
+        self.frames = [frame for (index, frame) in sorted(zip(indice, self.frames))]
     def append(self,frame):
         self.frames.append(frame)
     def analyze(self, norm_factor = 1.0):
@@ -51,37 +56,3 @@ class Animation:
         mean_y = np.mean(ys)
         self.x = map(lambda x:x-mean_x, xs)
         self.y = map(lambda y:y-mean_y, ys)
-
-#def scatter_plot(animations, path):
-#    plt.figure()
-#    for i,animation in enumerate(animations):
-#        x, y = zip(*animation.centers)
-#
-#        plt.scatter(        
-    
-
-if __name__ == "__main__":
-    kernal = np.ones((3,3), np.uint8)
-    background = cv2.imread("background20.bmp", cv2.IMREAD_GRAYSCALE)
-    image = cv2.imread("blocking7.bmp", cv2.IMREAD_GRAYSCALE)
-    result = cv2.addWeighted(image, 1.0, background, -1.0, 0)
-    result = cv2.blur(result, (5,5))
-    cv2.imwrite("result.bmp", result)
-    #ret, result2 = cv2.threshold(result, 30, 255, cv2.THRESH_BINARY)
-    result2 = cv2.adaptiveThreshold(result, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, -10)
-    cv2.imwrite("result2.bmp", result2)
-    circles = cv2.HoughCircles(result2, cv2.cv.CV_HOUGH_GRADIENT, 1, 5, param1=50, param2=20, minRadius = 10, maxRadius = 60)
-    result2 = cv2.cvtColor(result2, cv2.COLOR_GRAY2RGB)
-    circles = np.uint16(np.around(circles))
-    #circles = circles[0, :] 
-    circles = [min(circles[0, :].tolist(), key = lambda x: x[2])]
-    result = cv2.cvtColor(result, cv2.COLOR_GRAY2RGB)
-    for i in circles:
-        cv2.circle(result, (i[0], i[1]), i[2], (0,255,0), 2)
-        cv2.circle(result, (i[0], i[1]), 2, (0,255,255), 3)
-        cv2.circle(result2, (i[0], i[1]), i[2], (0,255,0), 2)
-        cv2.circle(result2, (i[0], i[1]), 2, (0,255,255), 3)
-    #result2 = cv2.adaptiveThreshold(result, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-    #                cv2.THRESH_BINARY, 31, -35)
-    cv2.imwrite("result3.bmp", result)
-    cv2.imwrite("result4.bmp", result2)
--- experiment_analysis.py
+++ experiment_analysis.py
@@ -21,51 +21,71 @@ def get_save_path(path, note = "result"):
 class SD_Protocol:
     def __init__(self, background_image_arr, blur_kernal = None, saved_results = None, circle_chooser = None, Hough_param1 = None, Hough_param2 = None, Hough_min_dist = None, minRad = None, maxRad = None, thresh_hold_func = None, thresh_hold = None, adapt_rad = None, adapt_plus = None ):
         self.background_image = background_image_arr
-        self.blur_kernal    = init_mVar(blur_kernal, np.ones((5,5), np.uint8))
-        self.saved_results  = init_mVar(saved_results, [])
-        self.circle_chooser = init_mVar(circle_chooser, choose_smallest_circle) 
-        self.Hough_min_dist = init_mVar(Hough_min_dist, 1)
-        self.Hough_param1   = init_mVar(Hough_param1, 50)
-        self.Hough_param2   = init_mVar(Hough_param2, 20)
-        self.minRad         = init_mVar(minRad, 10)
-        self.maxRad         = init_mVar(maxRad, 60)
-        self.thresh_hold    = init_mVar(thresh_hold, 30)
-        self.adapt_rad      = init_mVar(adapt_rad, 51)
-        self.adapt_plus     = init_mVar(adapt_plus, -10)
-        self.thresh_hold_func = init_mVar(thresh_hold_func, self.gaussian_thresh_hold)
+        self.blur_kernal    = SD_Protocol.init_mVar(blur_kernal, (5,5))
+        self.saved_results  = SD_Protocol.init_mVar(saved_results, [4])
+        self.circle_chooser = SD_Protocol.init_mVar(circle_chooser, SD_Protocol.choose_biggest_circle) 
+        self.Hough_min_dist = SD_Protocol.init_mVar(Hough_min_dist, 1)
+        self.Hough_param1   = SD_Protocol.init_mVar(Hough_param1, 50)
+        self.Hough_param2   = SD_Protocol.init_mVar(Hough_param2, 20)
+        self.minRad         = SD_Protocol.init_mVar(minRad, 10)
+        self.maxRad         = SD_Protocol.init_mVar(maxRad, 60)
+        self.thresh_hold    = SD_Protocol.init_mVar(thresh_hold, 30)
+        self.adapt_rad      = SD_Protocol.init_mVar(adapt_rad, 51)
+        self.adapt_plus     = SD_Protocol.init_mVar(adapt_plus, -10)
+        self.thresh_hold_func = SD_Protocol.init_mVar(thresh_hold_func, self.adaptive_thresh_hold)
         self.path           = None
         
     
     def __call__(self, frame):
         self.path = frame.path
+        print "Processing %s" %self.path
         frame.read()
         temp_image = np.copy(frame.image)
 
         temp_image = cv2.addWeighted(temp_image, 1.0, self.background_image, -1.0, 0)
+        subtracted_image = np.copy(temp_image)
         self.save(temp_image, "subtracted", 0)
 
-        temp_image = cv2.blur(temp_image, self.blur_kernal)
+        for i in range(3):
+            temp_image = cv2.blur(temp_image, self.blur_kernal )
         self.save(temp_image, "blurred", 1)
 
         temp_image = self.thresh_hold_func(temp_image)
         self.save(temp_image, "threshod", 2)
 
-        circles = cv2.HoughCircles(temp_image, cv2.cv.CV_HOUGH_GRADIENT, 1, self.Hough_min_dist, param1 = self.Hough_param1, param2 = self.Hough_param2, minRadius self.minRad, maxRadius = self.maxRad)
+        circles = None
+        grace = 0
+        while circles == None:
+            circles = cv2.HoughCircles(temp_image, cv2.cv.CV_HOUGH_GRADIENT, 1, self.Hough_min_dist, param1 = self.Hough_param1, param2 = self.Hough_param2 - grace, minRadius = self.minRad, maxRadius = self.maxRad)
+            grace +=1
+        circles = np.uint16(np.around(circles))
         x, y, r = self.circle_chooser(circles)
 
-        toRGB(temp_image)
-        draw_circle(temp_image, x, y, r)
-        self.save(temp_image, "circle", 3)
+        temp = np.copy(temp_image)
+        contours, garbage = cv2.findContours(temp, cv2.cv.CV_RETR_LIST, cv2.cv.CV_CHAIN_APPROX_NONE)
+        circles2 = map(cv2.minEnclosingCircle, contours)
+        (x, y), r = min(circles2, key = lambda x: abs(r-x[1]))
 
-        toRGB(frame.image)
-        draw_circle(frame.image, x, y, r)
-        self.save(frame.image, "circle_on_orig", 4)
+        x, y, r = map(lambda m: int(round(m)), [x,y,r])
+
+        subtracted_image= SD_Protocol.toRGB(subtracted_image)
+        SD_Protocol.draw_circle(subtracted_image, x, y, r)
+        self.save(subtracted_image, "circle", 3)
 
         frame.center = (x,y)
         frame.radius = r
+        frame.free()
+        del temp_image
+        temp_image = None
+
+    def adaptive_thresh_hold(self, image):
+        return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, self.adapt_rad, self.adapt_plus)
+
+    def global_thresh_hold(self, image):
+        return cv2.threshold(image, self.thresh_hold, 255, cv2.THRESH_BINARY)
 
         
-    def save(image, note, i):
+    def save(self,image, note, i):
         if i in self.saved_results:
             cv2.imwrite(get_save_path(self.path, note), image)
 
@@ -76,7 +96,7 @@ class SD_Protocol:
 
     @staticmethod
     def toRGB(image):
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
     @staticmethod
     def init_mVar(value, default):
@@ -92,8 +112,23 @@ class SD_Protocol:
     @staticmethod
     def choose_biggest_circle(circles):
         return max(circles[0, :].tolist(), key = lambda x:x[2])
+
+    @staticmethod
+    def choose_first_circle(circles):
+        return circles[0, :].tolist()[0]
+
     
 if __name__ == "__main__":
-    protocol0 = SD_Protocol()
-    exp_0 = Animation()
-    exp_0.import_frame("../Blocking0/blocking0.bmp", protocol0)
+    background = cv2.imread("background20.bmp", cv2.IMREAD_GRAYSCALE)
+    protocol = SD_Protocol(background, saved_results = range(4))
+    experiments = []
+    for i in range(8):
+        experiment = Animation()
+        experiment.import_seq("../Blocking%d/" % i, "blocking(\d+).bmp\Z", protocol)
+        experiment.analyze()
+        experiments.append(experiment)
+    plotscatter(experiments)
+    plotsd(experiments)
+    
+
+
